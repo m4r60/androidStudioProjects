c4846c5679ffb13aa55efebbb59e41e56ce4f3cb
marcoapefe <marcoapefe@hotmail.com>
2017-07-18 19:21:29 +0200

    Score Keeper App Resubmitted

--- Score Keeper App/Football Score Keeper App/app/src/main/java/com/example/android/estadisticasdelpartidodefutbol/MainActivity.java
+++ Score Keeper App/Football Score Keeper App/app/src/main/java/com/example/android/estadisticasdelpartidodefutbol/MainActivity.java
@@ -6,18 +6,16 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private int teamShotsA=0;
-    private int teamShotsB=0;
-    private int teamGoalsA =0;
-    private int teamGoalsB =0;
-    private int teamTotalCardsA=0;
-    private int teamTotalCardsB=0;
-    private int teamYellowCardsA=0;
-    private int teamYellowCardsB=0;
+    private int teamShotsA = 0;
+    private int teamShotsB = 0;
+    private int teamGoalsA = 0;
+    private int teamGoalsB = 0;
+    private int teamTotalCardsA = 0;
+    private int teamTotalCardsB = 0;
+    private int teamYellowCardsA = 0;
+    private int teamYellowCardsB = 0;
     private int teamRedCardsA = 0;
     private int teamRedCardsB = 0;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +25,12 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * Displays the given shots.
+     *
      * @param score of the team
      * @param teamA to know if we are going to change the shots of team A or team B.
      */
     public void displayForTeamShots(int score, boolean teamA) {
-        if (teamA){
+        if (teamA) {
             TextView scoreView = (TextView) findViewById(R.id.tema_a_shots);
             scoreView.setText(String.valueOf(score));
         } else {
@@ -42,21 +41,23 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * Displays the given goals.
+     *
      * @param goals of the team.
      * @param teamA to know if we are going to change the goals score of team A or team B.
      */
     public void displayForTeamGoals(int goals, boolean teamA) {
-       if(teamA){
+        if (teamA) {
             TextView scoreView = (TextView) findViewById(R.id.team_a_goals);
             scoreView.setText(String.valueOf(goals));
-       }else {
-           TextView scoreView = (TextView) findViewById(R.id.team_b_goals);
-           scoreView.setText(String.valueOf(goals));
-       }
+        } else {
+            TextView scoreView = (TextView) findViewById(R.id.team_b_goals);
+            scoreView.setText(String.valueOf(goals));
+        }
     }
 
     /**
      * Displays the total cards.
+     *
      * @param cards of the team.
      * @param teamA to know if we are going to change the cards of team A or team B.
      */
@@ -64,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
         if (teamA) {
             TextView scoreView = (TextView) findViewById(R.id.team_a_total_cards);
             scoreView.setText(String.valueOf(cards));
-        }else{
+        } else {
             TextView scoreView = (TextView) findViewById(R.id.team_b_total_cards);
             scoreView.setText(String.valueOf(cards));
         }
@@ -72,14 +73,15 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * Displays the given goals for Team a.
+     *
      * @param yellowCards of the team.
-     * @param teamA to know if we are going to change the yellow cards of team A or team B.
+     * @param teamA       to know if we are going to change the yellow cards of team A or team B.
      */
     public void displayForTeamYellowCards(int yellowCards, boolean teamA) {
         if (teamA) {
             TextView scoreView = (TextView) findViewById(R.id.team_a_yellow_cards);
             scoreView.setText(String.valueOf(yellowCards));
-        }else{
+        } else {
             TextView scoreView = (TextView) findViewById(R.id.team_b_yellow_cards);
             scoreView.setText(String.valueOf(yellowCards));
         }
@@ -87,14 +89,15 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * Displays the red cards.
+     *
      * @param redCards of the team.
-     * @param teamA to know if we are going to change the red cards of team A or team B.
+     * @param teamA    to know if we are going to change the red cards of team A or team B.
      */
     public void displayForTeamRedCards(int redCards, boolean teamA) {
-        if(teamA) {
+        if (teamA) {
             TextView scoreView = (TextView) findViewById(R.id.team_a_red_cards);
             scoreView.setText(String.valueOf(redCards));
-        }else{
+        } else {
             TextView scoreView = (TextView) findViewById(R.id.team_b_red_cards);
             scoreView.setText(String.valueOf(redCards));
         }
@@ -102,54 +105,54 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * Add 1 shot to team A.
+     *
      * @param view
      */
-    public void addShotsA (View view){
-
-            teamShotsA = teamShotsA + 1;
-            displayForTeamShots(teamShotsA, true);
+    public void addShotsA(View view) {
+        teamShotsA++;
+        displayForTeamShots(teamShotsA, true);
     }
 
     /**
      * Add 1 shot to team B.
+     *
      * @param view
      */
-    public void addShotsB (View view){
-
-        teamShotsB = teamShotsB + 1;
+    public void addShotsB(View view) {
+        teamShotsB++;
         displayForTeamShots(teamShotsB, false);
     }
 
     /**
      * Add 1 goal to team a.
+     *
      * @param view
      */
-    public void addGoalsA (View view){
-
+    public void addGoalsA(View view) {
         teamGoalsA = teamGoalsA + 1;
         displayForTeamGoals(teamGoalsA, true);
     }
 
     /**
      * Add 1 goal to team b.
+     *
      * @param view
      */
-    public void addGoalsB (View view){
-
-        teamGoalsB = teamGoalsB + 1;
+    public void addGoalsB(View view) {
+        teamGoalsB++;
         displayForTeamGoals(teamGoalsB, false);
 
     }
 
     /**
      * Add 1 yellow card to team a.
+     *
      * @param view
      */
 
-    public void addYellowCardsA (View view){
-
-        teamYellowCardsA = teamYellowCardsA+ 1;
-        teamTotalCardsA = teamTotalCardsA + 1;
+    public void addYellowCardsA(View view) {
+        teamYellowCardsA++;
+        teamTotalCardsA++;
         displayForTeamYellowCards(teamYellowCardsA, true);
         displayForTeamTotalCards(teamTotalCardsA, true);
 
@@ -157,12 +160,12 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * Add 1 yellow card to team b.
+     *
      * @param view
      */
-    public void addYellowCardsB (View view){
-
-        teamYellowCardsB = teamYellowCardsB + 1;
-        teamTotalCardsB = teamTotalCardsB + 1;
+    public void addYellowCardsB(View view) {
+        teamYellowCardsB++;
+        teamTotalCardsB++;
         displayForTeamYellowCards(teamYellowCardsB, false);
         displayForTeamTotalCards(teamTotalCardsB, false);
 
@@ -170,65 +173,58 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * Add 1 red card to team a.
+     *
      * @param view
      */
 
-    public void addRedCardsA (View view){
-
-        teamRedCardsA =teamRedCardsA+ 1;
-        teamTotalCardsA = teamTotalCardsA + 1;
-        displayForTeamRedCards(teamRedCardsA,true);
+    public void addRedCardsA(View view) {
+        teamRedCardsA++;
+        teamTotalCardsA++;
+        displayForTeamRedCards(teamRedCardsA, true);
         displayForTeamTotalCards(teamTotalCardsA, true);
-
-
     }
 
     /**
      * Add 1 red card to team b
+     *
      * @param view
      */
 
-    public void addRedCardsB (View view){
-
-        teamRedCardsB = teamRedCardsB + 1;
-        teamTotalCardsB = teamTotalCardsB + 1;
-        displayForTeamRedCards(teamRedCardsB,false);
+    public void addRedCardsB(View view) {
+        teamRedCardsB++;
+        teamTotalCardsB++;
+        displayForTeamRedCards(teamRedCardsB, false);
         displayForTeamTotalCards(teamTotalCardsB, false);
 
     }
 
     /**
-     * Team A: Resets all parameters.
+     * Resets all parameters for both teams.
+     *
      * @param view
      */
-
-   public void resetA (View view){
-       teamShotsA=0;
-       displayForTeamShots(teamShotsA, true);
-       teamGoalsA =0;
-       displayForTeamGoals(teamGoalsA, true);
-       teamTotalCardsA=0;
-       displayForTeamTotalCards(teamTotalCardsA, true);
-       teamYellowCardsA=0;
-       displayForTeamYellowCards(teamYellowCardsA, true);
-       teamRedCardsA = 0;
-       displayForTeamRedCards(teamRedCardsA,true);
-    }
-
-    /**
-     * Team B: Resets all parameters.
-     * @param view
-     */
-    public void resetB (View view){
-        teamShotsB=0;
+    public void reset(View view) {
+        //TEAM A
+        teamShotsA = 0;
+        displayForTeamShots(teamShotsA, true);
+        teamGoalsA = 0;
+        displayForTeamGoals(teamGoalsA, true);
+        teamTotalCardsA = 0;
+        displayForTeamTotalCards(teamTotalCardsA, true);
+        teamYellowCardsA = 0;
+        displayForTeamYellowCards(teamYellowCardsA, true);
+        teamRedCardsA = 0;
+        displayForTeamRedCards(teamRedCardsA, true);
+        //TEAM B
+        teamShotsB = 0;
         displayForTeamShots(teamShotsB, false);
-        teamGoalsB =0;
+        teamGoalsB = 0;
         displayForTeamGoals(teamGoalsB, false);
-        teamTotalCardsB=0;
+        teamTotalCardsB = 0;
         displayForTeamTotalCards(teamTotalCardsB, false);
-        teamYellowCardsB=0;
+        teamYellowCardsB = 0;
         displayForTeamYellowCards(teamYellowCardsB, false);
         teamRedCardsB = 0;
-        displayForTeamRedCards(teamRedCardsB,false);
+        displayForTeamRedCards(teamRedCardsB, false);
     }
 }
